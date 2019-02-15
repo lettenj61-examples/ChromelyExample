7ff0807afc75cfda531d0de2aa9389a8665313b2
lettenj61 <lettenj61@users.noreply.github.com>
2019-02-15 20:44:00 +0900

    start

---
+++ ChromelyExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using Chromely.CefSharp.Winapi.BrowserWindow;
+using Chromely.Core;
+using Chromely.Core.Infrastructure;
+using Chromely.Core.Host;
+
+namespace ChromelyExample
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string startUrl = "local://app/sample.html";
+
+            ChromelyConfiguration config = ChromelyConfiguration
+                .Create()
+                .WithHostMode(WindowState.Normal, true)
+                .WithHostTitle("chromely")
+                .WithAppArgs(args)
+                .WithHostSize(1024, 768)
+                .WithStartUrl(startUrl)
+                .WithLogSeverity(LogSeverity.Verbose)
+                .UseDefaultResourceSchemeHandler("local", string.Empty);
+
+            using (var window = new CefSharpBrowserWindow(config))
+            {
+                return window.Run(args);
+            }
+        }
+    }
+}
